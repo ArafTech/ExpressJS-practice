67a794451cc4161f3ca3fecb19d95ef489349697
Arafath Aleem <arafathaleem111@gmail.com>
2025-08-11 17:20:10 +0530

    Practice implementing Product CRUD operations in model

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const rootDir = require('./utils/path');
+const rootDir = require('./src/utils/path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -10,12 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
-app.use('/',shopRoutes);
+// app.use('/admin',adminRoutes);
+// app.use('/',shopRoutes);
+
+
 app.use((req, res) => {
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
-
 
 // Start the server
 app.listen(PORT, () => {
